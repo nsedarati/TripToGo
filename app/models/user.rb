5107e1c9256fbeb0c57c71fbb0b4c76aa9ef9bb1
Niloufar <n.sedarati@gmail.com>
2017-02-16 13:52:39 +0800

    Facebook Signin

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ActiveRecord::Base
+  has_many :authentications, :dependent => :destroy
+  has_secure_password
+  validates :email, presence: true, uniqueness: true,
+  format: {
+    with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/
+  }
+
+  before_save :downcase_email
+
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+    # byebug
+    user = User.create!(first_name: auth_hash["info"]["name"], email: auth_hash["extra"]["raw_info"]["email"],:password => auth_hash['uid'])
+    # user.password = "888888000"
+    # byebug
+    user.authentications << (authentication)
+
+    return user
+  end
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+  def password_optional?
+    true
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def generate_password_reset_token!
+    update_attribute(:password_reset_token, SecureRandom.urlsafe_base64(48))
+  end
+end
---
+++ db/migrate/20170214163721_change_password_required_for_users.rb
@@ -0,0 +1,5 @@
+class ChangePasswordRequiredForUsers < ActiveRecord::Migration
+ def change
+    change_column_null :users, :password_digest, true
+  end
+end
